28f4159dc698a6920833a7b5c0533d79dd93c80a
nemtinov010809 <114688245+nemtinov010809@users.noreply.github.com>
2023-02-19 18:07:42 +0300

    Add files via upload

---
+++ Задание 2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задание_2
+{
+    public partial class Lab6_2 : Form
+    {
+        public Lab6_2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            double sum = 0;
+            double summand = 0.0;
+            double x;
+            double precision;
+            bool f = false;
+            if (double.TryParse(txtPrecision.Text, out precision))
+            {
+                f = true;
+            }
+            do
+            {
+                counter++;
+                if (double.TryParse(txtX.Text, out x))
+                {
+                    summand = double.Parse(txtX.Text) / counter;
+                }
+                sum += summand;
+            }
+            while (f && Math.Abs(summand) > precision);
+            if (f)
+            {
+                lblResult.Text = "Сумма = " + sum + ", колличество = " + counter;
+            }
+            else
+            {
+                MessageBox.Show("Ввелите числовое значение!!!");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
